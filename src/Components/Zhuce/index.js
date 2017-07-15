dffb40da7ea1b7f76261d509d7d92d7d28fb703c
simonSha <897601562@qq.com>
2017-07-16 00:22:11 +0800

    zhece

---
+++ src/Components/Zhuce/index.js
@@ -0,0 +1,22 @@
+import React,{Component} from "react";
+import "./index.scss";
+
+class Zhuce extends Component{
+	constructor(props) {
+		super(props);
+		
+	}
+
+
+	render() {
+		return (
+
+			<div>
+
+					food
+			</div>
+		)
+	}
+}
+
+export default Zhuce;
