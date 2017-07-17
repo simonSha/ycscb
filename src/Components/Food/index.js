29506b7c3267c339058ec9c5a538f13a28dda89b
simonSha <897601562@qq.com>
2017-07-17 22:07:13 +0800

    "7.170701"

--- src/Components/Food/index.js
+++ src/Components/Food/index.js
@@ -25,7 +25,7 @@ class Food extends Component{
 
 	componentDidMount() {
 		axios.get("/index.php?act=list&op=getYouhuiClass&cate=meishi").then((res)=>{
-			 console.log(res.data.data.cate.data);
+			 console.log(res.data.data.cate);
 			// this.setState({
 			
 			// })
