d3c076c30ec69e49fc75537891c7276e45e8d165
simonSha <897601562@qq.com>
2017-07-17 09:23:47 +0800

    7.170403

---
+++ src/Components/Car/index.js
@@ -0,0 +1,22 @@
+import React,{Component} from "react";
+import "./index.scss";
+
+class Car extends Component{
+	constructor(props) {
+		super(props);
+		
+	}
+
+
+	render() {
+		return (
+
+			<div id="List">
+
+					list
+			</div>
+		)
+	}
+}
+
+export default Car;
