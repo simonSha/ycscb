a564a72717399365d5e49c4a05120dafc0330d3f
dayDayUpzl <924268957@qq.com>
2017-07-20 14:08:19 +0800

    "17/7/20df"

--- src/Components/Zhuce/index.js
+++ src/Components/Zhuce/index.js
@@ -14,12 +14,13 @@ class Zhuce extends Component{
 
 
 			<div id="zhuce">
+			<div className="top">
 				<from className="from">
 					<div className="call">
 						<div className="left">
 							<input ref="username" type="text" placeholder="请输入11位手机号码" className="leftt"/>
 						</div>
-
+						<p className="p">请输入正确手机</p>
 						<div className="right">
 							获取验证码
 						</div>
@@ -28,14 +29,17 @@ class Zhuce extends Component{
 					<div className="yz">
 						<input type="text" className="yzm" placeholder="请输入收到的短信验证码"/>
 					</div>
-
+					<p className="p">请输入验证码</p>
 					<div className="pw">
-						<input type="password" className="psw" placeholder="请输入您的mima，长度至少六位"/>
+						<input type="password" className="psw" placeholder="请输入您的密码，长度至少六位"/>
 						<button className="btn" onClick={this.hclick.bind(this)}>
 							显示
 						</button>
 					</div>
+					<p className="p">请输入您的密码</p>
 				</from>
+			</div>
+
 
 				<div className="wc">
 					<a className="a li1">
